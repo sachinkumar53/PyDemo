1c127bf0efefb974309e132791004e1c8d7d19d7
sachinkumar53 <kr771.sachin@gmail.com>
2023-07-05 20:43:32 +0530

    numpy demo

--- app/src/main/java/com/sachin/app/pydemo/MainActivity.kt
+++ app/src/main/java/com/sachin/app/pydemo/MainActivity.kt
@@ -2,7 +2,6 @@ package com.sachin.app.pydemo
 
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
@@ -12,26 +11,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         if (!Python.isStarted()) {
-            Python.start(AndroidPlatform(this));
+            Python.start(AndroidPlatform(this))
         }
         val result = findViewById<TextView>(R.id.result)
-        val num1 = findViewById<TextView>(R.id.num1)
-        val num2 = findViewById<TextView>(R.id.num2)
-        val btn = findViewById<TextView>(R.id.btn)
+        val arrView = findViewById<TextView>(R.id.arr_view)
+        val arr = arrayOf(1, 2, 3, 4, 5)
+        arrView.text = "[" + arr.joinToString() + "]"
 
-        btn.setOnClickListener {
-            val a = num1.text.toString().toIntOrNull()
-            val b = num2.text.toString().toIntOrNull()
-
-            if (a == null || b == null) {
-                Toast.makeText(this, "Enter value of a and b", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
+        try {
             val py = Python.getInstance()
-            val i = py.builtins.callAttr("sum", arrayOf(a, b)).toJava(Int::class.java)
-            result.text = i.toString()
+            val npy = py.getModule("demo")
+            val s = npy.callAttr("getArrSize", arr).toJava(Int::class.java)
+            result.text = "size: $s"
+        } catch (e: Exception) {
+            e.printStackTrace()
+            result.text = e.toString()
         }
     }
 }
--- app/src/main/python/demo.py
+++ app/src/main/python/demo.py
@@ -1,2 +1,8 @@
-def sum(a,b):
-    return a+b
+import numpy as np
+
+def getArrSize(arr):
+    arr1 = np.array(arr)
+    return arr1.size
+
+#exmaple method call which will return value 5
+#getArrSize([1,2,3,4,5])
